64634ab35167f50c39533dc9bc15d325dfc34987
MartinPokorny86 <pokornymartin2@gmail.com>
2024-06-23 12:55:57 +0200

    Created header with author's id

--- SQL_projekt.sql
+++ SQL_projekt.sql
@@ -1,3 +1,7 @@
 /*
- * 4. projekt: SQL
+ * SQL_projekt.sql: čtvrtý projekt do Engeto Online Python Akademie
+ * author: Martin Pokorný
+ * email: pokornymartin2@gmail.com
+ * discord: martin_pokorny86
 */
+
