a394eb888c0964f470e191d211b514da721ed657
MartinPokorny86 <pokornymartin2@gmail.com>
2024-06-23 17:45:50 +0200

    Assigned the definitions to code's czechia_payroll

--- SQL_projekt.sql
+++ SQL_projekt.sql
@@ -15,6 +15,10 @@ FROM czechia_payroll AS cp;
  * 100	fyzický
  * 200	přepočtený
  * 
+ * unit_code:
+ * 200		Kč
+ * 80403	tis. osob (tis. os.)
+ * 
  * value_type_code:
  * 316	Průměrný počet zaměstnaných osob
  * 5958	Průměrná hrubá mzda na zaměstnance
@@ -36,7 +40,7 @@ SELECT *
 FROM czechia_price AS cp;
 
 SELECT *
-FROM czechia_price_category AS cpc;
+FROM czechia_price_category AS cpcat;
 
 SELECT *
 FROM czechia_region AS cr;
@@ -49,3 +53,19 @@ FROM countries AS c;
 
 SELECT *
 FROM economies AS e;
+
+# přiřazení definic ke kódům do tabulky czechia_payroll
+
+SELECT
+	cp.id, cp.value, cp.value_type_code, cpvt.name AS value_type_name,
+	cp.unit_code, cpu.name AS unit_name, cp.calculation_code, cpc.name AS calculation_name, 
+	cp.industry_branch_code, cpib.name AS industry_branch_name, cp.payroll_year, cp.payroll_quarter 
+FROM czechia_payroll AS cp
+LEFT JOIN czechia_payroll_value_type AS cpvt
+	ON cp.value_type_code = cpvt.code
+LEFT JOIN czechia_payroll_unit AS cpu
+	ON cp.unit_code = cpu.code
+LEFT JOIN czechia_payroll_calculation AS cpc
+	ON cp.calculation_code = cpc.code
+LEFT JOIN czechia_payroll_industry_branch AS cpib
+	ON cp.industry_branch_code = cpib.code;
