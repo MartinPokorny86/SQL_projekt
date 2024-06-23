a99816c848d86f5a24e06259a94d6f56d4fd11af
MartinPokorny86 <pokornymartin2@gmail.com>
2024-06-23 22:53:15 +0200

    Added tables with overview of average month salary in Czechia by quartals and years

--- SQL_projekt.sql
+++ SQL_projekt.sql
@@ -69,3 +69,75 @@ LEFT JOIN czechia_payroll_calculation AS cpc
 	ON cp.calculation_code = cpc.code
 LEFT JOIN czechia_payroll_industry_branch AS cpib
 	ON cp.industry_branch_code = cpib.code;
+
+# přepočtená průměrná hrubá mzda na zaměstnance ve vybraném sektoru a roce - pouze pro kontrolu dat
+
+SELECT
+	cp.id, cp.value, cp.value_type_code, cpvt.name AS value_type_name,
+	cp.unit_code, cpu.name AS unit_name, cp.calculation_code, cpc.name AS calculation_name, 
+	cp.industry_branch_code, cpib.name AS industry_branch_name, cp.payroll_year, cp.payroll_quarter 
+FROM czechia_payroll AS cp
+LEFT JOIN czechia_payroll_value_type AS cpvt
+	ON cp.value_type_code = cpvt.code
+LEFT JOIN czechia_payroll_unit AS cpu
+	ON cp.unit_code = cpu.code
+LEFT JOIN czechia_payroll_calculation AS cpc
+	ON cp.calculation_code = cpc.code
+LEFT JOIN czechia_payroll_industry_branch AS cpib
+	ON cp.industry_branch_code = cpib.code
+WHERE cp.value_type_code = 5958 AND cp.calculation_code  = 200 AND cp.industry_branch_code = "E" AND cp.payroll_year = "2021";
+
+# přepočtená průměrná hrubá mzda na zaměstnance v Česku podle kvartálů jednotlivých let - podrobná tabulka (s kódy)
+
+SELECT
+	cp.payroll_year, cp.payroll_quarter, cp.value, cp.value_type_code, cpvt.name AS value_type_name,
+	cp.unit_code, cpu.name AS unit_name, cp.calculation_code, cpc.name AS calculation_name, 
+	cp.industry_branch_code, cpib.name AS industry_branch_name
+FROM czechia_payroll AS cp
+LEFT JOIN czechia_payroll_value_type AS cpvt
+	ON cp.value_type_code = cpvt.code
+LEFT JOIN czechia_payroll_unit AS cpu
+	ON cp.unit_code = cpu.code
+LEFT JOIN czechia_payroll_calculation AS cpc
+	ON cp.calculation_code = cpc.code
+LEFT JOIN czechia_payroll_industry_branch AS cpib
+	ON cp.industry_branch_code = cpib.code
+WHERE cp.value_type_code = 5958 AND cp.calculation_code  = 200 AND cp.industry_branch_code IS NULL
+GROUP BY cp.industry_branch_code, cp.payroll_year, cp.payroll_quarter;
+
+# přepočtená průměrná hrubá mzda na zaměstnance v Česku podle kvartálů jednotlivých let - zjednodušená tabulka
+
+SELECT
+	cp.payroll_year, cp.payroll_quarter, cp.value AS avg_salary, CONCAT(cpu.name, ' / ', 'měsíc') AS unit_name,
+	cpvt.name AS value_type_name, cpc.name AS calculation_name
+FROM czechia_payroll AS cp
+LEFT JOIN czechia_payroll_value_type AS cpvt
+	ON cp.value_type_code = cpvt.code
+LEFT JOIN czechia_payroll_unit AS cpu
+	ON cp.unit_code = cpu.code
+LEFT JOIN czechia_payroll_calculation AS cpc
+	ON cp.calculation_code = cpc.code
+LEFT JOIN czechia_payroll_industry_branch AS cpib
+	ON cp.industry_branch_code = cpib.code
+WHERE cp.value_type_code = 5958 AND cp.calculation_code  = 200 AND cp.industry_branch_code IS NULL
+GROUP BY cp.industry_branch_code, cp.payroll_year, cp.payroll_quarter;
+
+# průměrná hrubá mzda na zaměstnance (fyzické osoby) v Česku podle kvartálů jednotlivých let - zjednodušená tabulka
+
+SELECT
+	cp.payroll_year, cp.payroll_quarter, cp.value AS avg_salary, CONCAT(cpu.name, ' / ', 'měsíc') AS unit_name,
+	cpvt.name AS value_type_name, cpc.name AS calculation_name
+FROM czechia_payroll AS cp
+LEFT JOIN czechia_payroll_value_type AS cpvt
+	ON cp.value_type_code = cpvt.code
+LEFT JOIN czechia_payroll_unit AS cpu
+	ON cp.unit_code = cpu.code
+LEFT JOIN czechia_payroll_calculation AS cpc
+	ON cp.calculation_code = cpc.code
+LEFT JOIN czechia_payroll_industry_branch AS cpib
+	ON cp.industry_branch_code = cpib.code
+WHERE cp.value_type_code = 5958 AND cp.calculation_code  = 100 AND cp.industry_branch_code IS NULL
+GROUP BY cp.industry_branch_code, cp.payroll_year, cp.payroll_quarter;
+
+
+	
