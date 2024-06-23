3c7019f9e4c4f98e51dd56c2be7eafa26d356896
MartinPokorny86 <pokornymartin2@gmail.com>
2024-06-23 13:03:22 +0200

    Add source data sets

--- SQL_projekt.sql
+++ SQL_projekt.sql
@@ -5,3 +5,37 @@
  * discord: martin_pokorny86
 */
 
+# Zdrojové datové sady
+
+SELECT *
+FROM czechia_payroll AS cp;
+
+SELECT *
+FROM czechia_payroll_calculation AS cpc;
+
+SELECT *
+FROM czechia_payroll_industry_branch AS cpib;
+
+SELECT *
+FROM czechia_payroll_unit AS cpu;
+
+SELECT *
+FROM czechia_payroll_value_type AS cpvt;
+
+SELECT *
+FROM czechia_price AS cp;
+
+SELECT *
+FROM czechia_price_category AS cpc;
+
+SELECT *
+FROM czechia_region AS cr;
+
+SELECT *
+FROM czechia_district AS cd;
+
+SELECT *
+FROM countries AS c;
+
+SELECT *
+FROM economies AS e;
