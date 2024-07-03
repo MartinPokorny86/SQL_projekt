6eb7fd9f769bcff3c1c80d467c8d1a6c1577b8cd
MartinPokorny86 <pokornymartin2@gmail.com>
2024-07-03 10:47:16 +0200

    Add all industry branch codes and their names to czechia_payroll, for codes null: CZ, for names null: Czechia, previously was condition for only industry branch code is null

--- SQL_projekt.sql
+++ SQL_projekt.sql
@@ -92,7 +92,8 @@ WHERE cp.value_type_code = 5958 AND cp.calculation_code  = 200 AND cp.industry_b
 SELECT
 	cp.payroll_year, cp.payroll_quarter, cp.value, cp.value_type_code, cpvt.name AS value_type_name,
 	cp.unit_code, cpu.name AS unit_name, cp.calculation_code, cpc.name AS calculation_name, 
-	cp.industry_branch_code, cpib.name AS industry_branch_name
+	cp.industry_branch_code, COALESCE (cpib.name, 'Czechia') AS industry_branch_name,
+	COALESCE (cp.industry_branch_code, 'CZ')
 FROM czechia_payroll AS cp
 LEFT JOIN czechia_payroll_value_type AS cpvt
 	ON cp.value_type_code = cpvt.code
@@ -102,7 +103,7 @@ LEFT JOIN czechia_payroll_calculation AS cpc
 	ON cp.calculation_code = cpc.code
 LEFT JOIN czechia_payroll_industry_branch AS cpib
 	ON cp.industry_branch_code = cpib.code
-WHERE cp.value_type_code = 5958 AND cp.calculation_code  = 200 AND cp.industry_branch_code IS NULL
+WHERE cp.value_type_code = 5958 AND cp.calculation_code  = 200
 GROUP BY cp.industry_branch_code, cp.payroll_year, cp.payroll_quarter;
 
 # přepočtená průměrná hrubá mzda na zaměstnance v Česku podle kvartálů jednotlivých let - zjednodušená tabulka
@@ -190,3 +191,9 @@ FROM czechia_price AS cp;
 
 SELECT *
 FROM czechia_region AS cr;
+
+
+
+
+
+
