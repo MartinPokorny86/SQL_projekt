acc0d0ce71193a6c0ceeaa90b4c072d86b6f74e2
MartinPokorny86 <pokornymartin2@gmail.com>
2024-07-04 20:16:15 +0200

    Processing part of the third task

--- SQL_projekt.sql
+++ SQL_projekt.sql
@@ -390,7 +390,25 @@ ORDER BY cp.category_code, cp.date_to, cp.region_code ASC;
  * Řešení: 
  */
 
-
+SELECT
+	ROW_NUMBER() OVER (PARTITION BY cp.category_code ORDER BY cp.date_to) AS category_row_nr,	
+	YEAR(cp.date_to) AS date_to_year, 
+	cp.category_code, cpc.name AS category_name, 
+	cpc.price_value, cpc.price_unit,
+	ROUND(AVG(cp.value), 2) AS average_year_price,
+	CASE 
+		WHEN ROW_NUMBER() OVER (PARTITION BY cp.category_code ORDER BY cp.date_to) BETWEEN 1 AND 12 
+		AND CONCAT(YEAR(cp.date_to), '_', cp.category_code, '_', ROW_NUMBER() OVER (PARTITION BY cp.category_code ORDER BY cp.date_to)) != '2018_212101_4'
+		THEN ROUND(((LEAD(AVG(cp.value), 1) OVER (ORDER BY cp.category_code) / AVG(cp.value)) * 100),2) - 100
+		ELSE '*'
+	END AS next_vs_previous_pct
+FROM czechia_price AS cp
+LEFT JOIN czechia_price_category AS cpc
+	ON cp.category_code = cpc.code
+LEFT JOIN czechia_region AS cr
+	ON cp.region_code = cr.code
+GROUP BY cp.category_code, YEAR(cp.date_to)
+ORDER BY cp.category_code, cp.date_to ASC;
 
 
 
