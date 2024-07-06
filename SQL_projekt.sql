d67e68233f421103b3dc6c2598d03f4b1e6a730d
MartinPokorny86 <pokornymartin2@gmail.com>
2024-07-06 22:27:56 +0200

    The fifth question has been answered

--- SQL_projekt.sql
+++ SQL_projekt.sql
@@ -545,4 +545,109 @@ GROUP BY cpay.industry_branch_code, cpay.payroll_year;
  * Otázka č. 5: Má výška HDP vliv na změny ve mzdách a cenách potravin? 
  * Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
  * Řešení: 
+ * 
+ * Vliv HDP na změny ve mzdách:
+ * 
+ * Největší nárůst HDP v Česku byl v období let 2000 až 2020 zaznamenán mezi roky 2006 a 2005, kdy se v roce 2006 zvýšilo HDP oproti předchozímu roku o 6,8 %.
+ * Mzdy se v roce 2006 zvýšily o 6,5 % oproti roku 2005.
+ * 
+ * V roce 2007 došlo oproti roku 2006 k mírnějšímu nárůstu HDP o 5,6 %. I přes menší nárůst HDP než tomu bylo při porovnání let 2006 a 2005 (o cca 1 %) došlo 
+ * v roce 2007 oproti roku 2006 k výraznějšímu nárůstu mezd o 7,2 %.
+ * 
+ * V roce 2018 se HDP oproti roku 2017 zvýšilo o 3,2 %. A v roce 2018 došlo k výraznému růstu mezd oproti roku 2017 o 8,2 %.
+ * 
+ * Závěrem lze říci, že výraznější růst HDP nemá pravidelný vliv na výrazný růst mezd.
+ * 
+ * Vliv HDP na změny v cenách potravin
+ * 
+ * Z finální tabulky, kterou jsem použil ve třetí otázce, je patrné, že nejmenší meziroční růst cen byl zaznamenán u rajských jablek červených kulatých.
+ * V roce 2007 došlo ke snížení jejich ceny oproti roku 2006 o 30,3 %. 
+ * 
+ * V roce 2018 se HDP oproti roku 2017 zvýšilo o 3,2 %. Změna výše HDP byla v tomto období nižší než mezi lety 2007 a 2006, kdy činila 5,6 %.
+ * 
+ * Při porovnání s rokem 2017 cena rajských jablek v roce 2018 klesla o 0,5 %.
+ * 
+ * V roce 2007 bylo největší zdražení zaznamenáno u paprik, konkrétně o 94,81 % oproti roku 2006.
+ * 
+ * Při porovnání s rokem 2017 cena paprik v roce 2018 klesla o 3,1 %
+ * 
+ * V roce 2007 byla pšeničná mouka hladká dražší o 22,6 % než tomu bylo v roce 2006. V té době byl nárůst HDP o 5,6 %. 
+ * Naopak v roce 2018 byla hladká mouka z pšenice o 0,05 % dražší než v roce 2017. Mouka tak byla v roce 2018 cenově dostupnější, byť v té době bylo HDP
+ * o 3,2 % větší než v minulém roce.
+ * 
+ * Závěrem lze říci, že rajčata byla levnější při vyšším meziročním růstu HDP (porovnání let 2007/2006) a dražší při nižším meziročním růstu HDP (porovnání let 2018/2017).
+ * Naopak papriky byly při vyšším meziročním růstu HDP v roce 2007 mnohem dražší než v roce 2018, kdy meziroční změna HDP činila 3,2 %.
+ * Pšeničná mouka hladká byla v rámci porovnání let 2007/2006 dražší než při porovnání let 2018/2017.
+ * Výše HDP má tedy různý vliv na cenu u jednotlivých kategorií potravin.
+ * 
  */
+
+SELECT
+	e.country, e.`year`, e.GDP,
+	((LEAD(e.GDP, 1) OVER (ORDER BY e.`year`) / (e.GDP)) * 100) - 100 AS gdp_next_vs_previous_pct
+FROM economies AS e
+WHERE country = 'Czech Republic' AND e.YEAR >= 1990
+ORDER BY e.`year` ASC;
+
+# pro analýzu dat vlivu meziročního nárůstu hdp a mezd používám tabulku B z řešení čtvrté otázky:
+
+# Tabulka B: přepočtená průměrná hrubá mzda na zaměstnance v Česku podle čtvrtletí jednotlivých let - zjednodušená tabulka
+
+SELECT
+	cpay.payroll_year,
+	COALESCE (cpay.industry_branch_code, 'CZ') AS industry_branch_code, COALESCE (cpib.name, 'Czechia') AS industry_branch_name,
+	round(avg(cpay.value),0) AS avg_salary, CONCAT(cpu.name, ' / ', 'měsíc') AS unit_name,
+	cpvt.name AS value_type_name, cpc.name AS calculation_name,
+	((LEAD(AVG(cpay.value), 1) OVER (ORDER BY cpay.payroll_year) / AVG(cpay.value)) * 100) - 100 AS next_vs_previous_pct, 
+	e.YEAR AS gdp_year, e.country,
+	((LEAD(e.GDP, 1) OVER (ORDER BY e.`year`) / (e.GDP)) * 100) - 100 AS gdp_next_vs_previous_pct
+FROM czechia_payroll AS cpay
+LEFT JOIN czechia_payroll_value_type AS cpvt
+	ON cpay.value_type_code = cpvt.code
+LEFT JOIN czechia_payroll_unit AS cpu
+	ON cpay.unit_code = cpu.code
+LEFT JOIN czechia_payroll_calculation AS cpc
+	ON cpay.calculation_code = cpc.code
+LEFT JOIN czechia_payroll_industry_branch AS cpib
+	ON cpay.industry_branch_code = cpib.code
+LEFT JOIN economies AS e 
+	ON cpay.payroll_year = e.`year` 
+WHERE cpay.value_type_code = 5958 AND cpay.calculation_code  = 200 AND cpay.industry_branch_code IS NULL AND e.country = 'Czech Republic'
+GROUP BY cpay.industry_branch_code, cpay.payroll_year;
+
+# pro analýzu dat vlivu meziročního nárůstu hdp a cen potravin používám tabulku "finální kód" z řešení třetí otázky:
+
+SELECT 
+    category_row_nr,
+    date_to_year, 
+    category_code, 
+    category_name, 
+    price_value, 
+    price_unit, 
+    average_year_price, 
+    next_vs_previous_pct
+FROM (
+	SELECT
+		ROW_NUMBER() OVER (PARTITION BY cp.category_code ORDER BY cp.date_to) AS category_row_nr,	
+		YEAR(cp.date_to) AS date_to_year, 
+		cp.category_code, cpc.name AS category_name, 
+		cpc.price_value, cpc.price_unit,
+		ROUND(AVG(cp.value), 2) AS average_year_price,
+		CASE 
+			WHEN ROW_NUMBER() OVER (PARTITION BY cp.category_code ORDER BY cp.date_to) BETWEEN 1 AND 12 
+			AND CONCAT(YEAR(cp.date_to), '_', cp.category_code, '_', ROW_NUMBER() OVER (PARTITION BY cp.category_code ORDER BY cp.date_to)) != '2018_212101_4'
+			THEN ROUND(((LEAD(AVG(cp.value), 1) OVER (ORDER BY cp.category_code) / AVG(cp.value)) * 100),2) - 100
+			ELSE NULL 
+		END AS next_vs_previous_pct
+	FROM czechia_price AS cp
+	LEFT JOIN czechia_price_category AS cpc
+		ON cp.category_code = cpc.code
+	LEFT JOIN czechia_region AS cr
+		ON cp.region_code = cr.code
+	GROUP BY cp.category_code, YEAR(cp.date_to)
+) sub
+	ORDER BY  
+    	next_vs_previous_pct IS NULL,
+		next_vs_previous_pct, 
+    	category_code, 
+    	date_to_year;
